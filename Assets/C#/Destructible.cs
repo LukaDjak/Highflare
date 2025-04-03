bbe5b316ecc0cdc6480c920726d018ca1b37d6b7
DJ4K <dluka0240@gmail.com>
2025-04-03 14:10:53 +0200

    Apr 3 | Grid Pain & URP
    - converted all materials to URP (at least i think so)
    - created customizable grid shader
    - implemented destructible script for doors and glass

---
+++ Assets/C#/Destructible.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Destructible: MonoBehaviour
+{
+    [Header("Destructible Object Settings")]
+    [SerializeField] private GameObject fracturedObjectPrefab;
+    [SerializeField] private float destructionVelocityThreshold = 15f;
+    [SerializeField] private bool canBeDestroyedByPlayer = true;
+
+    private bool isDestroyed = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isDestroyed)
+            return;
+
+        if (collision.relativeVelocity.magnitude > destructionVelocityThreshold && collision.gameObject.CompareTag("Grabbable"))
+            DestroyObject();
+        if (canBeDestroyedByPlayer && collision.gameObject.CompareTag("Player"))
+            DestroyObject();
+        if (collision.gameObject.CompareTag("Bullet"))
+            DestroyObject();
+    }
+
+    private void DestroyObject()
+    {
+        Debug.Log("Fracture");
+        //if (fracturedObjectPrefab != null)
+            //Instantiate(fracturedObjectPrefab, transform.position, transform.rotation);
+        //Destroy(gameObject);
+        isDestroyed = true;
+    }
+}
